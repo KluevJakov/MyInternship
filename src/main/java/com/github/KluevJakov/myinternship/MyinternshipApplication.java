77db390cbac35168623d932826a35c4e7b7b9deb
KluevJakov <kluevja@gmail.com>
2021-09-22 14:54:50 +0400

    Empty project

---
+++ src/main/java/com/github/KluevJakov/myinternship/MyinternshipApplication.java
@@ -0,0 +1,13 @@
+package com.github.KluevJakov.myinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyinternshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/KluevJakov/myinternship/MyinternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.KluevJakov.myinternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
