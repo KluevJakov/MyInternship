c0653c4a4622a03371426983fafd22397750db77
KluevJakov <kluevja@gmail.com>
2021-09-24 19:19:05 +0400

    Lexical fix

--- src/main/java/com/github/KluevJakov/myinternship/tasks/StreamTask.java
+++ src/main/java/com/github/KluevJakov/myinternship/tasks/StreamTask.java
@@ -4,17 +4,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamTask {
-    public static Map<Integer, List<String>> groupByDigitNumbers(int[] input){
+    public static Map<Integer, List<String>> groupByDigitNumbers (int[] input) {
 
         Map<Integer, List<String>> result = Arrays.stream(input)
                 .boxed()
                 .filter(e -> e > 0)
                 .sorted()
-                .map(e -> e%2==0 ? "e"+e : "o"+e)
+                .map(e -> e % 2 == 0 ? "e" + e : "o" + e)
                 .collect(Collectors.toMap(
-                        e -> e.length()-1,
+                        e -> e.length() - 1,
                         list -> new ArrayList<String>(Arrays.asList(list)),
-                        (v1,v2) -> {
+                        (v1, v2) -> {
                             v1.addAll(v2);
                             return v1;
                         }
