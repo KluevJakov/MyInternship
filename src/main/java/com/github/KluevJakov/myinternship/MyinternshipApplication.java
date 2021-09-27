b10c2424c52f27488046ed583b28235f1416a94f
KluevJakov <kluevja@gmail.com>
2021-09-27 06:08:12 +0400

    StreamTask and UTs for it Fix StreamTask class Remove useless .boxed Fixes in UTs StreamConvertTask with groupingBy Lexical and ut fix Naming fix

--- src/main/java/com/github/KluevJakov/myinternship/MyinternshipApplication.java
+++ src/main/java/com/github/KluevJakov/myinternship/MyinternshipApplication.java
@@ -1,14 +1,7 @@
 package com.github.KluevJakov.myinternship;
 
-import com.github.KluevJakov.myinternship.tasks.DatatypesTask;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class MyinternshipApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(MyinternshipApplication.class, args);
 	}
 
 }
