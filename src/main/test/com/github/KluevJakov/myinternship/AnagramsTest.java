70bff8ce273a2b27e0c264be1eda77464c6b8a72
KluevJakov <kluevja@gmail.com>
2021-09-27 06:51:58 +0400

    AnagramsTask and UTs for it Algorithm based on Map of Frequency

---
+++ src/main/test/com/github/KluevJakov/myinternship/AnagramsTest.java
@@ -0,0 +1,33 @@
+package com.github.KluevJakov.myinternship;
+
+import com.github.KluevJakov.myinternship.AnagramsTask;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnagramsTest {
+    @Test
+    public void lowerCheck() {
+        assertTrue(AnagramsTask.isAnagram("anagram","margana"));
+    }
+
+    @Test
+    public void upperCheck() {
+        assertTrue(AnagramsTask.isAnagram("ANAGRAM","MARGANA"));
+    }
+
+    @Test
+    public void differentCheck() {
+        assertTrue(AnagramsTask.isAnagram("aNaGrAm","MaRgAnA"));
+    }
+
+    @Test
+    public void notAnagramCheck() {
+        assertFalse(AnagramsTask.isAnagram("aNaGrHm","MaRgAnA"));
+    }
+
+    @Test
+    public void differentSizeCheck() {
+        assertFalse(AnagramsTask.isAnagram("aNaGrAmA","MaRgAnA"));
+    }
+}
